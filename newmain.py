9c56656f98d00cc6f444d8409454328524e4f382
WolfProg7 <maildoguifigueiredo@gmail.com>
2025-03-13 19:50:25 +0000

    Update newmain.py

--- newmain.py
+++ newmain.py
@@ -7,11 +7,37 @@ import os
 pygame.init()
 
 screendims = [9 * 50, 16 * 50] # [450, 800]
-displayw, displayh = screendims # Display dimensions
+displayw, displayh = pygame.display.Info().current_w, pygame.display.Info().current_h # Display dimensions
 
 def displayper(wh: int, per: int) -> int:
     """Function that returns a percentage of the screen's width or height in pixels"""
     return displayw * (per / 100) if wh == 0 else displayh * (per / 100)
+
+class bin:
+    class cfgs:
+        colors = json.loads(open("./bin/cfgs/colors.cfg", "r").read())
+        text = json.loads(open("./bin/cfgs/text.cfg", "r").read())
+        for size in text["sizes"]: # The original text size is in percentage, this for loop converts it into pixels
+            text["sizes"][size] = round((displayper(0, text["sizes"][size]) + displayper(1, text["sizes"][size])) / 2)
+    class icons:
+        account = pygame.transform.scale(pygame.image.load("./bin/icons/account.png"), (displayper(1, 8), displayper(1, 8)))
+        information = pygame.transform.scale(pygame.image.load("./bin/icons/information.png"), (displayper(1, 8), displayper(1, 8)))
+        investments = pygame.transform.scale(pygame.image.load("./bin/icons/investments.png"), (displayper(1, 8), displayper(1, 8)))
+        menu = pygame.transform.scale(pygame.image.load("./bin/icons/menu.png"), (displayper(1, 5), displayper(1, 5)))
+        back = pygame.transform.scale(pygame.image.load("./bin/icons/back.png"), (displayper(1, 5), displayper(1, 5)))
+        backarrow = pygame.transform.scale(pygame.image.load("./bin/icons/backarrow.png"), (displayper(1, 6), displayper(1, 6)))
+        frontarrow = pygame.transform.scale(pygame.image.load("./bin/icons/frontarrow.png"), (displayper(1, 6), displayper(1, 6)))
+    stocks = {}
+    for stock in os.listdir("./bin/stocks/"):
+        fields = []
+        rows = []
+
+        with open("./bin/stocks/" + stock, "r") as file:
+            csvreader = csv.reader(file)
+            fields = next(csvreader)
+            for row in csvreader:
+                rows.append(row)
+        stocks[stock[::-1][4:][::-1]] = rows
 
 def ismouseinrect(mousepos: list[int, int], rect: list[int, int, int, int] | pygame.Rect) -> bool:
     """Function that return a bool if the mouse's position in on top of a rect"""
@@ -22,7 +48,14 @@ def ismouseinrect(mousepos: list[int, int], rect: list[int, int, int, int] | pyg
     return False
 
 def getTopleftFromMiddle(middle: list[int, int], surfaceSize: list[int, int] | pygame.Surface):
-    pass
+    mx, my = middle
+    if type(surfaceSize) == list:
+        top = my - (surfaceSize[1] // 2)
+        left = mx - (surfaceSize[0] // 2)
+    else:
+        top = my - (surfaceSize.get_height() // 2)
+        left = mx - (surfaceSize.get_width() // 2)
+    return [left, top]
 
 def checkEvents(events: list) -> dict:
     """Function to check window events"""
@@ -37,7 +70,9 @@ def drawMenuBar(display: pygame.Surface, page: int) -> None:
     barHeight = displayper(1, 12.5)
     coords = [0, displayh - barHeight] # Top left coordenates
     
-    pygame.draw.rect(display, bin.cfgs.colors["menubackground"], pygame.Rect(coords[0], coords[1], displayw - coords[0], displayh - coords[1]))
+    pygame.draw.rect(display, bin.cfgs.colors["menubackground"], pygame.Rect(coords[0], coords[1], displayw - coords[0], displayh - coords[1])) # Menu rectangle
+
+    display.blit(text())
 
 def main() -> None:
     """Main function, displaying the window, drawing the main objects, ..."""
@@ -65,32 +100,6 @@ def main() -> None:
             pass
 
         pygame.display.flip() # Clearing the screen
-
-class bin:
-    class cfgs:
-        colors = json.loads(open("./bin/cfgs/colors.cfg", "r").read())
-        text = json.loads(open("./bin/cfgs/text.cfg", "r").read())
-        for size in text["sizes"]: # The original text size is in percentage, this for loop converts it into pixels
-            text["sizes"][size] = round((displayper(0, text["sizes"][size]) + displayper(1, text["sizes"][size])) / 2)
-    class icons:
-        account = pygame.transform.scale(pygame.image.load("./bin/icons/account.png"), (displayper(1, 8), displayper(1, 8)))
-        information = pygame.transform.scale(pygame.image.load("./bin/icons/information.png"), (displayper(1, 8), displayper(1, 8)))
-        investments = pygame.transform.scale(pygame.image.load("./bin/icons/investments.png"), (displayper(1, 8), displayper(1, 8)))
-        menu = pygame.transform.scale(pygame.image.load("./bin/icons/menu.png"), (displayper(1, 5), displayper(1, 5)))
-        back = pygame.transform.scale(pygame.image.load("./bin/icons/back.png"), (displayper(1, 5), displayper(1, 5)))
-        backarrow = pygame.transform.scale(pygame.image.load("./bin/icons/backarrow.png"), (displayper(1, 6), displayper(1, 6)))
-        frontarrow = pygame.transform.scale(pygame.image.load("./bin/icons/frontarrow.png"), (displayper(1, 6), displayper(1, 6)))
-    stocks = {}
-    for stock in os.listdir("./bin/stocks/"):
-        fields = []
-        rows = []
-
-        with open("./bin/stocks/" + stock, "r") as file:
-            csvreader = csv.reader(file)
-            fields = next(csvreader)
-            for row in csvreader:
-                rows.append(row)
-        stocks[stock[::-1][4:][::-1]] = rows
 
 # NOTE: Replace screendims with [displayw, displayh] before compiling
 display = pygame.display.set_mode(screendims) # Screen dimensions
